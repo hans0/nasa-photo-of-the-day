6ed0bb8d788ea7ebf60c753438eeffe2fd927784
hans0 <kiessler.hans@gmail.com>
2021-02-18 18:48:32 -0500

    Had trouble with it, but I was able to get the navbar all Styled Component'd up

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,41 @@
 import React from "react";
-import './NavBar.css';
+// import './NavBar.css';
+import styled, { keyframes } from 'styled-components';
 
-function NavBar() {
+
+const StyledNavBar = styled.div`
+  /* border: 1px solid black; */
+  /* border: 1px solid ${props => props.theme.primaryColor}; */
+  color: ${props => props.theme.primaryColor};
+  display: flex;
+  flex-direction: row;
+  
+  div {
+    /* border: 1px solid black; */
+    font-weight: bold;
+    padding: 10px;
+    flex-grow: 1;
+    :hover {
+      /* color: 'grey'; */
+      color: ${props => props.theme.secondaryColor};
+      background-color: ${props => props.theme.quaternaryColor};
+      transition: all 0.5s ease-in-out;
+    }
+    transition: all 0.5s ease-in-out;
+  }
+`;
+
+function NavBar({theme}) {
   return (
     <nav>
       <h3>NASA Photo of the Day</h3>
-      <div className="container">
-        <div>Home</div>
+      {/* <div className="menu"> */}
+      <StyledNavBar theme={theme}>
         <div>Yesterday</div>
-      </div>
+        <div>Today</div>
+        <div>Tomorrow</div>
+      </StyledNavBar>
+      {/* </div> */}
     </nav>
   )
 }
---
+++ src/theme/index.js
@@ -0,0 +1,15 @@
+export default {
+    primaryColor: '#1A1A1D',
+    secondaryColor: '#4E4E50',
+    tertiaryColor: '#6F2232',
+    quaternaryColor: '#950740',
+    pentaryColor: '#C3073F',
+    dangerColor: 'red',
+    black: 'black',
+    white: 'white',
+    breakpointMobile: '(max-width: 550px)',
+    padding: {
+      small: '4px',
+      large: '12px',
+    }
+  }
